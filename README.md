dd0bbff0748b7bc8098e4423542603a2f86379d9
aayushxrj <aayush.ranjan0904@gmail.com>
2025-09-16 19:08:09 +0530

    initial setup

---
+++ README.md
@@ -0,0 +1,48 @@
+# COMMANDS
+
+```
+go clean -modcache
+```
+```
+go mod tidy
+```
+```
+go mod verify
+```
+
+# gRPC Gateway
+
+```
+go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@latest
+go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@latest
+```
+
+
+```
+mkdir -p third_party
+cd third_party
+git clone https://github.com/googleapis/googleapis.git
+cd ..
+```
+
+Add to protoc command
+```
+--grpc-gateway_out=proto/gen --grpc-gateway_opt=paths=source_relative
+--openapiv2_out=proto/gen
+```
+
+```
+protoc \
+  -I proto \
+  -I third_party/googleapis \
+  --go_out=proto/gen --go_opt=paths=source_relative \
+  --go-grpc_out=proto/gen --go-grpc_opt=paths=source_relative \
+  --validate_out="lang=go,paths=source_relative:proto/gen" \
+  --grpc-gateway_out=proto/gen --grpc-gateway_opt=paths=source_relative \
+  --openapiv2_out=proto/gen \
+  proto/main.proto
+```
+
+```
+go get github.com/grpc-ecosystem/grpc-gateway/v2@latest
+```
---
+++ server.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+
+	mainpb "github.com/aayushxrj/gRPC-rest-combo-api/proto/gen"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
+
+	_ "google.golang.org/grpc/encoding/gzip"
+	"google.golang.org/grpc/metadata"
+)
+
+type server struct {
+	mainpb.UnimplementedCalculatorServer
+}
+
+func (s *server) GenerateFibonacci(req *mainpb.FibonacciRequest, stream mainpb.Calculator_GenerateFibonacciServer) error {
+	// Validate request
+	if err := req.Validate(); err != nil {
+		return status.Errorf(codes.InvalidArgument, "validation failed: %v", err)
+	}
+
+	ctx := stream.Context()
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		log.Println("No metadata recieved")
+	}
+	fmt.Println("Metadata recieved:", md)
+	val, ok := md["authorization"]
+	if !ok {
+		log.Println("No metadata recieved")
+	}
+	log.Println("Authorization:", val)
+
+	// Response headers to client
+	responseHeaders := metadata.Pairs("test", "testing1", "test2", "testing2")
+	if err := stream.SendHeader(responseHeaders); err != nil {
+		return err
+	}
+
+	n := req.GetN()
+	a, b := 0, 1
+
+	for i := 0; i < int(n); i++ {
+		err := stream.Send(&mainpb.FibonacciResponse{
+			Number: int32(a),
+		})
+		log.Println("Sent number:", a)
+		if err != nil {
+			return err
+		}
+		a, b = b, a+b
+		time.Sleep(time.Second)
+	}
+
+	trailer := metadata.New(map[string]string{
+		"end-status":   "completed",
+		"processed-by": "fibonacci-service",
+	})
+	stream.SetTrailer(trailer)
+
+	return nil
+}
+
+func (s *server) SendNumbers(stream mainpb.Calculator_SendNumbersServer) error {
+	var sum int32
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&mainpb.NumberResponse{Sum: sum})
+		}
+		if err != nil {
+			return err
+		}
+
+		// Validate each incoming request
+		if err := req.Validate(); err != nil {
+			return status.Errorf(codes.InvalidArgument, "validation failed: %v", err)
+		}
+
+		log.Println(req.GetNumber())
+		sum += req.GetNumber()
+	}
+}
+
+func (s *server) Chat(stream mainpb.Calculator_ChatServer) error {
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		// Validate incoming chat messages
+		if err := req.Validate(); err != nil {
+			return status.Errorf(codes.InvalidArgument, "validation failed: %v", err)
+		}
+
+		log.Println("Received Message:", req.GetMessage())
+
+		err = stream.Send(&mainpb.ChatMessage{
+			Message: req.GetMessage(),
+		})
+		if err != nil {
+			return err
+		}
+	}
+	fmt.Println("Returning control")
+	return nil
+}
+
+func main() {
+	port := ":50051"
+	cert := "cert.pem"
+	key := "key.pem"
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal("Failed to listen:", err)
+	}
+
+	creds, err := credentials.NewServerTLSFromFile(cert, key)
+	if err != nil {
+		log.Fatal("Failed to load credentials:", err)
+	}
+	grpcServer := grpc.NewServer(grpc.Creds(creds))
+
+	mainpb.RegisterCalculatorServer(grpcServer, &server{})
+
+	// enable reflection
+	reflection.Register(grpcServer)
+
+	log.Printf("Server is running on the port %s", port)
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		log.Fatal("Failed to serve:", err)
+	}
+}
